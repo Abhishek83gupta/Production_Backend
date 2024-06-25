c9f1f13a24ab8210fa2ef655cb028b93a51a7c04
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-06-25 21:57:57 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+app.use(express.json({limit:"16kb"}))     // Accepting json data // for form data
+app.use(express.urlencoded({extended:true,limit:"16kb"}))             // data coming form url  // for url data
+app.use(express.static("public"))   // for store images,pdf,etc
+app.use(cookieParser()) // for cookies
+
+
+export  {app}
---
+++ src/index.js
@@ -0,0 +1,75 @@
+// const dotenv = require('dotenv').config({path:' ./env'})    // for common js 
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:'./.env'
+})                     // config method which takes an object
+ 
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT || 8000,()=>{
+     console.log(`Server is running on port ${process.env.PORT}`);
+   })
+})
+.catch((err)=>{
+  console.log(`MONGO DB connection Failed !!!!`,err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from 'mongoose'
+// import { DB_NAME } from './constant.js'
+
+// import express from 'express'
+// const app = express()
+// ( async ()=>{
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/                   
+//     ${DB_NAME}`)
+//     app.on("error",(error)=>{           // Express listener 
+//         console.log("ERROR:",error);
+//         throw error
+//     })            
+
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//     })
+//   } catch (error) {
+//     console.error("Error",error)
+//     throw err
+//   } 
+// })()
+
+
